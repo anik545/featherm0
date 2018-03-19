529cc68e326855c32c9a4cdfbfedbaa0f2c03266
Anik Roy <anik545@gmail.com>
2018-03-19 17:47:31 +0000

    .

--- intruder/intruder/settings.py
+++ intruder/intruder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5&ufvqlnd$f@fhov0^z(k0hlxyo((yuo_-ir9(vxr9+h^s$9c('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['greenhack.pythonanywhere.com','198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['intruder.pythonanywhere.com','198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
